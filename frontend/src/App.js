7ae0742213553549e27e1e103f46a20abf7818cd
felDL1994 <59562121+felDL1994@users.noreply.github.com>
2020-03-23 14:46:03 +0100

    HTML 1.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Logo from "./assets/images/logo.png";
+import "./App.css";
 
 function App() {
   const [data, setData] = useState({});
@@ -14,13 +16,50 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(data);
+  /*   console.log(data);
+   */
+
   return (
     <div className="App">
       {isLoading === true ? (
-        <p>En cours de chargement ...</p>
+        <h1 className="loading">En cours de chargement ...</h1>
       ) : (
-        <div>{data.restaurant.name}</div>
+        <div>
+          <div className="header">
+            {" "}
+            <div className="wrapper">
+              <img className="logo" src={Logo} alt="" />
+            </div>
+          </div>
+          <div className="wrapper">
+            <h1>{data.restaurant.name}</h1>
+            <div className="restaurant-info">
+              <p>{data.restaurant.description}</p>
+              <img src={data.restaurant.picture} alt="" />
+            </div>
+          </div>
+
+          <div className="content">
+            <div className="wrapper">
+              <div className="details">
+                <div className="menus">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Excepturi quis nam modi voluptas pariatur dicta velit quasi,
+                    fugit aliquam hic nulla aut culpa assumenda? Debitis et
+                    laboriosam vero at aliquid.
+                  </p>
+                </div>
+                <div className="basket">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maiores molestias corporis ab vitae corrupti eum quia optio!
+                  Aut voluptatibus modi pariatur rerum ab sapiente ex accusamus
+                  soluta odio delectus! Possimus.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
