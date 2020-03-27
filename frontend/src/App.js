bedc1c9e55fdfeb89e9aa31ce242ad95e22dc46b
felDL1994 <59562121+felDL1994@users.noreply.github.com>
2020-03-27 11:38:35 +0100

    css 1.5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import axios from "axios";
 import Logo from "./assets/images/logo.png";
 import Category from "./components/Category";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "./App.css";
 
-library.add(faStar);
+library.add(faStar, faPlus, faMinus);
 
 function App() {
   const [data, setData] = useState({});
@@ -81,57 +83,79 @@ function App() {
               </div>
               <div className="panier">
                 <div className="panier-card">
-                  <button className="panier-valider">Valider</button>
-                  {selectedProducts.map((selectedProduct, index) => {
-                    return (
-                      <div className="panier-produits">
-                        <div>
-                          <p>
-                            <button
-                              onClick={() => {
-                                const newSelectedProducts = [
-                                  ...selectedProducts
-                                ];
-                                /*                              alert(index);
-                                 */
-                                if (newSelectedProducts[index].quantity === 1) {
-                                  newSelectedProducts.splice(index, 1);
-                                } else {
-                                  newSelectedProducts[index].quantity--;
-                                }
+                  <button className="panier-valider">Valider mon panier</button>
 
-                                setSelectedProducts(newSelectedProducts);
-                              }}
-                            >
-                              -
-                            </button>
-                            {selectedProduct.quantity}
-                            <button
-                              onClick={() => {
-                                const newSelectedProducts = [
-                                  ...selectedProducts
-                                ];
-                                /*                              alert(index);
-                                 */
+                  <div className="panier-choix">
+                    {selectedProducts.map((selectedProduct, index) => {
+                      return (
+                        <div className="panier-produits">
+                          <div className="panier-counter">
+                            <span>
+                              <button
+                                className="minus-button"
+                                onClick={() => {
+                                  const newSelectedProducts = [
+                                    ...selectedProducts
+                                  ];
+                                  /*                              alert(index);
+                                   */
+                                  if (
+                                    newSelectedProducts[index].quantity === 1
+                                  ) {
+                                    newSelectedProducts.splice(index, 1);
+                                  } else {
+                                    newSelectedProducts[index].quantity--;
+                                  }
 
-                                newSelectedProducts[index].quantity++;
-                                setSelectedProducts(newSelectedProducts);
-                              }}
-                            >
-                              +
-                            </button>
+                                  setSelectedProducts(newSelectedProducts);
+                                }}
+                              >
+                                <FontAwesomeIcon icon="minus"></FontAwesomeIcon>
+                              </button>
+                            </span>
+                            <span>{selectedProduct.quantity}</span>
+                            <span>
+                              <button
+                                className="add-button"
+                                onClick={() => {
+                                  const newSelectedProducts = [
+                                    ...selectedProducts
+                                  ];
+                                  /*                              alert(index);
+                                   */
+
+                                  newSelectedProducts[index].quantity++;
+                                  setSelectedProducts(newSelectedProducts);
+                                }}
+                              >
+                                <FontAwesomeIcon icon="plus"></FontAwesomeIcon>
+                              </button>
+                            </span>
+                          </div>
+                          <span className="panier-item-name">
                             {selectedProduct.title}
-                          </p>
+                          </span>
+                          <span className="panier-prix">
+                            {selectedProduct.price} €
+                          </span>
                         </div>
-                      </div>
-                    );
-                  })}
-                  <div className="panier-resume">
-                    <p>Sous-total: {total.toFixed(2)}</p>
-                    <p>Frais de livraison: 2,50€</p>
+                      );
+                    })}
+                  </div>
+
+                  <div className="panier-resultat">
+                    <div className="panier-sous-total">
+                      <span className="panier-text">Sous-total</span>
+                      <span className="panier-prix">{total.toFixed(2)} €</span>
+                    </div>
+                    <div className="panier-fraisdelivraison">
+                      <span className="panier-text">Frais de livraison</span>
+                      <span className="panier-prix">2.50 €</span>
+                    </div>
                   </div>
                   <div className="panier-total">
-                    <p>Total : {total.toFixed(2)}</p>
+                    <span className="panier-text">Total </span>
+                    <span className="panier-prix">{total.toFixed(2)}</span>
                   </div>
                 </div>
               </div>
